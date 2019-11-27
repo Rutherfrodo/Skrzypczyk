1432aec76e2270ae93bee8b55b25edb30ef03a27
Rutherfrodo <rutherfrodo@gmail.com>
2019-11-27 11:26:42 +0100

    Próba naprawy scrolla w skrzypczyku

--- kody/skrypt.js
+++ kody/skrypt.js
@@ -6,14 +6,17 @@ window.onload = function () {
 
 
   kontaktbutton.onclick = function(){
-    var element = document.getElementById("footer");
-    element.scrollIntoView({block:'nearest',behavior:'smooth',inline:'end'});
+    //var element = document.getElementById("footer");
+    //element.scrollIntoView({block:'nearest',behavior:'smooth',inline:'end'});
+    document.getElementById("footer").scrollIntoView( );
   };
 
   onasbutton.onclick = function(){
-    var element = document.getElementById("ofe");
-    element.scrollIntoView({block:'nearest',behavior:'smooth',inline:'start'});
-    
+    var el = document.getElementById("ofe1");
+    el.scrollIntoView(true);
+    document.getElementById("ofe").scrollTop -= 500;
+    //var element = document.getElementById("ofe");
+    //element.scrollIntoView({block:'nearest',behavior:'smooth',inline:'start'});
   };
   // TODO: Poprawa scrolli, są niedokładne. Źle nakierowuje na elementy strony
 
